0e15f7726b467594dc7f66871e935a5510dc1d60
Jakkha23 <jakob.khaytan@skola.botkyrka.se>
2020-10-19 14:01:38 +0200

    solved 6 (i think)

---
+++ 6/script.js
@@ -0,0 +1,11 @@
+function isSameUser(a, b) {
+    if (a === b) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+console.log(isSameUser("jakkha19@gmail.com", "jakkha19@gmail.com")) //returnerar true
+console.log(isSameUser("jakkha19@gmail.com", "jakob.khaytan@skola.botkyrka.se")) //returnerar false
